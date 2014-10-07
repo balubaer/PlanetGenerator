287254ef20fd0d3b9b6220afe485ba8012edba44
Bernd Niklas <niklas@win7-bn-mbpro.fritz.box>
2014-10-08 01:45:03 +0200

    Initial Commit

---
+++ PlanetGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PlanetGenerator
+//
+//  Created by Bernd Niklas on 08.10.14.
+//  Copyright (c) 2014 Bernd Niklas. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
