a1e791ecf22fceeadedc233eb3127ee0877a00a7
Bernd Niklas <niklas@win7-bn-mbpro.fritz.box>
2014-10-15 01:59:24 +0200

    Einführung von FleetFactory

---
+++ PlanetGenerator/FleetFactory.swift
@@ -0,0 +1,17 @@
+//
+//  FleetFactory.swift
+//  PlanetGenerator
+//
+//  Created by Bernd Niklas on 12.10.14.
+//  Copyright (c) 2014 Bernd Niklas. All rights reserved.
+//
+
+import Foundation
+
+class FleetFactory {
+    var dice: Dice
+    
+    init() {
+        dice = Dice()
+    }
+}
