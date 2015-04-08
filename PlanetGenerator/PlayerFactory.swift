5aa907419988cd0770fb25ced9d70307acadc328
Bernd <bernd_niklas@web.de>
2015-04-08 08:53:58 +0200

    PlayerFactory an die Implementierung von Java angepasst. Geht nicht mehr in die Endoskop bei großen Systemen.

--- PlanetGenerator/PlayerFactory.swift
+++ PlanetGenerator/PlayerFactory.swift
@@ -11,14 +11,19 @@ import Foundation
 class PlayerFactory {
     var planetDice: Dice
     var fleetDice: Dice
+    var playerDice: Dice
+    var distanceLevelHomes: Int
     var playerNameArray: Array <String>
     var passedPlanets: Array <Planet> = Array()
     var nextLevelPlanets: Array <Planet> = Array()
+    var homePlanetsDict: [String: Planet] = Dictionary()
 
     init(aPlayerNameArray: Array <String>) {
         planetDice = Dice()
         fleetDice = Dice()
+        playerDice = Dice()
         playerNameArray = aPlayerNameArray
+        distanceLevelHomes = 0
     }
     
     func findPlanetWithDice(dice:Dice, planetArray:Array <Planet>) -> Planet {
@@ -54,57 +59,40 @@ class PlayerFactory {
         return (fleet!, planet!)
     }
     
+    func findePlayerWithDice() -> Player {
+        var result = Player()
+        playerDice.setSites(playerNameArray.count)
+        var index = playerDice.roll() - 1
+        var playerName =  playerNameArray.removeAtIndex(index)
+        result.name = playerName
+        return result
+    }
+
+    
     func createWithPlanetArray(planetArray:Array <Planet>, fleetCount: Int, aFleetsOnHomePlanet: Int, startShipsCount: Int, distanceLevelHomes: Int) {
         var counter = 1
         planetDice.setSites(planetArray.count)
         fleetDice.setSites(fleetCount)
-        
-        for name in playerNameArray {
+        let playerNameCount: Int = playerNameArray.count;
+        self.distanceLevelHomes = distanceLevelHomes
+
+        for i in 1...playerNameCount {
             var fleetsOnHomePlanet = aFleetsOnHomePlanet
-            var player = Player()
-            var planet: Planet
-            
-            player.name = name
-
-            if counter == 1 {
+            var player = findePlayerWithDice();
+            var planet : Planet;
+            if (counter == 1) {
                 planet = findPlanetWithDice(planetDice, planetArray: planetArray)
-                var distLevel = DistanceLevel(aStartPlanet: planet, aDistanceLevel: distanceLevelHomes)
-                self.passedPlanets = distLevel.passedPlanets
-                self.nextLevelPlanets = distLevel.nextLevelPlanets
             } else {
-                planet = findPlanetWithDice(planetDice, planetArray: self.nextLevelPlanets)
-                var distLevel = DistanceLevel(aStartPlanet: planet, aDistanceLevel: distanceLevelHomes)
-                for planetFromPassedPlanets in distLevel.passedPlanets {
-                    if contains(self.passedPlanets, planetFromPassedPlanets) != true {
-                        self.passedPlanets.append(planetFromPassedPlanets)
-                    }
-                }
-                
-                var removePlanets: Array <Planet> = Array()
-                for planetFromNextLevel in self.nextLevelPlanets {
-                    if contains(self.passedPlanets, planetFromNextLevel) {
-                        removePlanets.append(planetFromNextLevel)
-                    }
-                }
-                for removePlanet in removePlanets {
-                    self.nextLevelPlanets.removeObject(removePlanet)
-                }
-
-                for planetFromNextLevel in distLevel.nextLevelPlanets {
-                    if contains(self.passedPlanets, planetFromNextLevel) != true {
-                        if contains(self.nextLevelPlanets, planetFromNextLevel) != true {
-                            self.nextLevelPlanets.append(planetFromNextLevel)
-                        }
-                    }
-                }
+                nextLevelPlanets = makeNextLevelPlanets();
+                planet = findPlanetWithDice(planetDice, planetArray: nextLevelPlanets);
             }
-            planet.player = player
+            planet.player = player;
+            homePlanetsDict[player.name] = planet;
             
             fleetsOnHomePlanet -= planet.fleets.count
-            
             for fleet in planet.fleets {
-                fleet.player = player
-                fleet.ships = startShipsCount
+                fleet.player = player;
+                fleet.ships = startShipsCount;
             }
             
             for index in 1...fleetsOnHomePlanet {
@@ -114,7 +102,49 @@ class PlayerFactory {
                 fleetAndPlanet.planet.fleets.removeObject(fleetAndPlanet.fleet)
                 planet.fleets.append(fleetAndPlanet.fleet)
             }
-            counter++
+
+            counter++;
+
         }
     }
+    
+    func makeNextLevelPlanets() -> Array <Planet> {
+        var result = Array<Planet>()
+        var allPassedPlanets = Array<Planet>()
+        var allNextLevelPlanets = Array<Planet>()
+        var finishCreate = false
+        var startDistanceLevelHomes = distanceLevelHomes
+        
+        while finishCreate == false {
+            for planet in  homePlanetsDict.values {
+                var distLevel = DistanceLevel(aStartPlanet: planet, aDistanceLevel: startDistanceLevelHomes);
+                for planetFromPassedPlanets in distLevel.passedPlanets {
+                    if contains(allPassedPlanets, planetFromPassedPlanets) == false {
+                        allPassedPlanets.append(planetFromPassedPlanets)
+                    }
+                }
+                for planetFromNextLevel in distLevel.nextLevelPlanets {
+                    if contains(allNextLevelPlanets, planetFromNextLevel) == false{
+                        allNextLevelPlanets.append(planetFromNextLevel)
+                    }
+                }
+                for planetFromNextLevel in allNextLevelPlanets {
+                    if contains(allPassedPlanets, planetFromNextLevel) == false{
+                        result.append(planetFromNextLevel)
+                    }
+                }
+            }
+            if result.count > 0 {
+                finishCreate = true
+            } else {
+                startDistanceLevelHomes--;
+                allPassedPlanets.removeAll()
+                allNextLevelPlanets.removeAll()
+            }
+        }
+        distanceLevelHomes = startDistanceLevelHomes;
+        return result;
+    }
+
+    
 }
